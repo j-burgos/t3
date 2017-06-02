436fffc506240664ac527b5e362dc1aad507e5b0
Jorge Burgos <jburgosk8@gmail.com>
2017-06-02 02:23:57 -0600

    Adds home screen component

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import './Home.css'
+
+export default class Home extends Component {
+  render () {
+    return (
+      <div className="home">
+        <h1 className="main-title">Tic Tac Toe</h1>
+        <div className="button-container">
+          <Link to="/game" className="primary">Start</Link>
+        </div>
+        <p className="copyright">© 2017 Jorge Burgos</p>
+      </div>
+    )
+  }
+}
