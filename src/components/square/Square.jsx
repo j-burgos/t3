7ab61f6357f964e3c9ca4b514cc1e38aba36789c
Jorge Burgos <jburgosk8@gmail.com>
2017-06-04 14:16:47 -0600

    Improves board square styling

--- src/components/square/Square.jsx
+++ src/components/square/Square.jsx
@@ -6,8 +6,11 @@ import './Square.css'
 
 export default class Square extends Component {
   render () {
-    const { value, onClick } = this.props
-    const player = value === 1 ? <PlayerX /> : <PlayerO />
+    const { isWinner, value, onClick } = this.props
+    const playerX = <PlayerX isWinner={isWinner} />
+    const playerO = <PlayerO isWinner={isWinner} />
+    const isPlayerX = value === 1
+    const player = isPlayerX ? playerX : playerO
     const valueRep = value === 0 ? '' : player
     return (
       <div className='square' onClick={onClick}>
