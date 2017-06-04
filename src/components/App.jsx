4e36fc4fe6b91d381707b7b0233084e205b58289
Jorge Burgos <jburgosk8@gmail.com>
2017-06-04 14:14:39 -0600

    Improves screen styles
    Adds screen background
    Improves screen transitions

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-
+import {CSSTransitionGroup} from 'react-transition-group'
 import {
   BrowserRouter as Router,
   Route,
@@ -14,13 +14,24 @@ class App extends Component {
   render () {
     return (
       <Router>
-        <Switch>
-          <Route exact path='/' component={Home} />
-          <Route path='/play' render={() => <Game boardSize={3} />} />
-          <Route path='/replay'>
-            <Redirect to='/play' />
-          </Route>
-        </Switch>
+        <Route path='/' render={({location}) => (
+          <div className='screen-container'>
+            <CSSTransitionGroup
+              transitionName="fade"
+              transitionEnterTimeout={500}
+              transitionLeaveTimeout={500}>
+              <Switch key={location.key} location={location}>
+                <Route exact path='/' component={Home} />
+                <Route path='/play'>
+                  <Game boardSize={3} />
+                </Route>
+                <Route path='/replay'>
+                  <Redirect to='/play'/>
+                </Route>
+              </Switch>
+            </CSSTransitionGroup>
+          </div>
+        )} />
       </Router>
     )
   }
