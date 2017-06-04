0d347b30e5d62414e1e9ed9beb25ee5b227a6986
Jorge Burgos <jburgosk8@gmail.com>
2017-06-04 02:24:01 -0600

    Improves marks by adding animation

--- src/components/player/PlayerX.jsx
+++ src/components/player/PlayerX.jsx
@@ -6,7 +6,7 @@ export default class PlayerX extends Component {
     return (
       <svg className='mark' viewBox='0 0 100 100'>
         <line className='line' x1='20' y1='20' x2='80' y2='80' />
-        <line className='line' x1='20' y1='80' x2='80' y2='20' />
+        <line className='line crossing' x1='20' y1='80' x2='80' y2='20' />
       </svg>
     )
   }
