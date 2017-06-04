c165b7d2283ea30fdc85717793297466bf1b6d9b
Jorge Burgos <jburgosk8@gmail.com>
2017-06-04 10:56:27 -0600

    Improves player marks
    Adds color for each player
    Improves drawing mark animation

--- src/components/player/PlayerO.jsx
+++ src/components/player/PlayerO.jsx
@@ -3,8 +3,10 @@ import './Player.css'
 
 export default class PlayerO extends Component {
   render () {
+    const winnerClass = this.props.isWinner ? 'win' : ''
+    const className = `mark o ${winnerClass}`
     return (
-      <svg className='mark' viewBox='0 0 100 100'>
+      <svg className={className} viewBox='0 0 100 100'>
         <circle className='line' cx={50} cy={50} r={30} />
       </svg>
     )
--- src/components/player/PlayerX.jsx
+++ src/components/player/PlayerX.jsx
@@ -3,10 +3,12 @@ import './Player.css'
 
 export default class PlayerX extends Component {
   render () {
+    const winnerClass = this.props.isWinner ? 'win' : ''
+    const className = `mark x ${winnerClass}`
     return (
-      <svg className='mark' viewBox='0 0 100 100'>
+      <svg className={className} viewBox='0 0 100 100'>
         <line className='line' x1='20' y1='20' x2='80' y2='80' />
-        <line className='line crossing' x1='20' y1='80' x2='80' y2='20' />
+        <line className='line crossing' x1='80' y1='20' x2='20' y2='80' />
       </svg>
     )
   }
